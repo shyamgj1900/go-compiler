8afad99da196771b73e1a820e69cdf9e63ca395e
Shyam <e0544484@u.nus.edu>
2024-03-27 14:23:12 +0800

    Code refactor

--- compiler/code.go
+++ compiler/code.go
@@ -1,14 +1,23 @@
 package main
 
 func plus(a int, b int) int {
-    a = b + 1
-    b = b + 2
-    return a + b
+    var z = 3
+    var x = 2
+    a = a * z
+    x = x + b
+    return a - x
+}
+
+func anotherFunction(a int) int{
+    var x = 2
+    x = x + a
+    return x * 3
 }
 
 func main() {
 
     var a = 2
     var res = plus(1, 2)
+    var res2 = anotherFunction(2)
 
 }
