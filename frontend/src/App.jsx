c525df851379e8bb10a2e671f06dd506574de2f0
Shyam <e0544484@u.nus.edu>
2024-03-31 16:17:22 +0800

    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState, useRef } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import CodeEditor from './components/CodeEditor';
+import './App.css'
+
+
+function App() {
+  
+  return (
+   <div className="m-2">
+      <CodeEditor />
+   </div>
+  )
+}
+
+export default App
